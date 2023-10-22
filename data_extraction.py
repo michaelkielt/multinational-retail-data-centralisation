d3370b9611c1fa4855dca26f2de146f2d1046e69
40156896 <michaelkielt@gmail.com>
2023-10-22 17:48:59 +0100

    Extract and read data from connected db

--- data_extraction.py
+++ data_extraction.py
@@ -8,7 +8,7 @@ an API and an S3 bucket
 from sqlalchemy import create_engine, inspect
 from database_utils import DataBaseConnector
 
-# Create instance of database_utils class to connect
+# Creates instance of database_utils class to connect
 # to db and generate engine
 db_connector = DataBaseConnector()
 db_engine = db_connector.init_db_engine()
@@ -18,13 +18,33 @@ class DataExtractor:
         self.engine = db_engine
 
     def list_db_tables(self):
-        # Create Inspector object to list table names
+        # Creates Inspector object to list table names
         inspector = inspect(self.engine)
         table_names = inspector.get_table_names()
         return table_names
     
     
     
-    #def read_rds_tables():
+    def read_rds_tables(self, table_name):
+        # Use db engine to execute a SELECT query on returned tables
+        with self.engine.connect() as connection:
+            query = f"SELECT * FROM {table_name}"
+            result = connection.execute(query)
+            data = result.fetchall()
+            return data
 
+# Creates data extractor instance with db engine
 data_extractor = DataExtractor(db_engine)
+
+# Lists tables in the connected database
+tables = data_extractor.list_db_tables()
+print("Tables in your database: ")
+for table in tables:
+    print(table)
+
+
+# Read data from a specific table (replace 'your_table_name' with an actual table name)
+table_data = data_extractor.read_rds_tables('legacy_store_details')
+print("Data from this table: ")
+for row in table_data:
+    print(row)    
--- database_utils.py
+++ database_utils.py
@@ -3,26 +3,20 @@ from sqlalchemy import create_engine
 
 class DataBaseConnector:
     
-
-    def read_db_creds():
+    # Read in the db_creds YAML and output as dict
+    def read_db_creds(self):
         with open("db_creds.yaml", "r") as file:
             db_creds = yaml.safe_load(file)
 
         return db_creds
     
+    # Create engine from the read_db_creds method
+    def init_db_engine(self):
+        db_config = self.read_db_creds()
+        db_url = f"postgresql+psycopg2://{db_config['RDS_USER']}:{db_config['RDS_PASSWORD']}@{db_config['RDS_HOST']}:{db_config['RDS_PORT']}/{db_config['RDS_DATABASE']}"
 
-    def init_db_engine():
-        db_config = DataBaseConnector.read_db_creds()
-        db_url = create_engine(
-            dialect="postgresql+psycopg2",
-            username=db_config['aicore_admin'],
-            password=db_config['AiCore2022'],
-            host=db_config['data-handling-project-readonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com'],
-            port=db_config['5432'],
-            database=db_config['postgres']
-        )
-
-        return db_url
+        engine = create_engine(db_url)
+        return engine
 
         
         
