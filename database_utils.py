4c75affb2ce50973ae309db8e4a1bc3f33a2a7f8
40156896 <michaelkielt@gmail.com>
2023-10-22 14:45:51 +0100

    Adds create engine func to create engine-db creds

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,5 @@
 import yaml
+from sqlalchemy import create_engine
 
 class DataBaseConnector:
     
@@ -9,4 +10,19 @@ class DataBaseConnector:
 
         return db_creds
     
+
     def init_db_engine():
+        db_config = DataBaseConnector.read_db_creds()
+        db_url = create_engine(
+            dialect="postgresql",
+            username=db_config['aicore_admin'],
+            password=db_config['AiCore2022'],
+            host=db_config['data-handling-project-readonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com'],
+            port=db_config['5432'],
+            database=db_config['postgres']
+        )
+
+        return db_url
+
+        
+        
