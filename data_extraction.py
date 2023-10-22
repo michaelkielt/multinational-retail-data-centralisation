fb07cd29e2dd11dc85446c7403ac9e8e7dd9d8dc
40156896 <michaelkielt@gmail.com>
2023-10-22 13:51:25 +0100

    Adds initial class files for project

---
+++ data_extraction.py
@@ -0,0 +1,9 @@
+'''
+This class will work as a utility class, in it you will be creating 
+methods that help extract data from different data sources.
+The methods contained will be fit to extract data from a #
+particular data source, these sources will include CSV files, 
+an API and an S3 bucket
+'''
+
+class DataExtractor: 
