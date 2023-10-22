0de3a5a17a7bfc06017a1875362bf39c2bd7857a
40156896 <michaelkielt@gmail.com>
2023-10-22 14:21:05 +0100

    Adds contect manager to read db creds

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,12 @@
+import yaml
+
 class DataBaseConnector:
     
+
+    def read_db_creds():
+        with open("db_creds.yaml", "r") as file:
+            db_creds = yaml.safe_load(file)
+
+        return db_creds
+    
+    def init_db_engine():
